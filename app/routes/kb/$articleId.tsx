5fb7c9fddfd2f358877b865d982f3a9bcf43175a
Mathieu Steele <matt@steeleagency.com>
2022-05-21 22:08:06 -0400

    prompt user before deleting

--- app/routes/kb/$articleId.tsx
+++ app/routes/kb/$articleId.tsx
@@ -63,7 +63,17 @@ export default function ArticleDetailsPage() {
           Edit
         </Link>
       </div>
-      <Form method="post" className="inline px-2">
+      <Form
+        method="post"
+        className="inline px-2"
+        onSubmit={(e) =>
+          window.confirm(
+            "Are you sure you want to delete? This cannot be undone"
+          )
+            ? ""
+            : e.preventDefault()
+        }
+      >
         <button
           type="submit"
           className="rounded bg-blue-500  py-2 px-4 text-white hover:bg-blue-600 focus:bg-blue-400"
